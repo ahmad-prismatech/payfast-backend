77a4c46fd0cbc2108165469031103e1e22ebc336
Ahmad Raza <ahmad.raza@prismatech.agency>
2022-11-02 16:21:12 +0500

    generate token api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+const crypto = require("crypto");
 
 const colors = require("./loaders/colors");
 const config = require("./config/config");
@@ -11,6 +13,44 @@ require("./loaders/routes")(app);
 
 app.get("/", (req, res) => {
   res.send("initial route running");
+});
+
+app.post("/signature", async (req, res) => {
+  try {
+    const { data, passPhrase } = req.body;
+    // Create parameter string
+    let pfOutput = "";
+    for (let key in data) {
+      if (data.hasOwnProperty(key)) {
+        if (data[key] !== "") {
+          pfOutput += `${key}=${encodeURIComponent(data[key].trim()).replace(
+            /%20/g,
+            "+"
+          )}&`;
+        }
+      }
+    }
+
+    // Remove last ampersand
+    let getString = pfOutput.slice(0, -1);
+    if (passPhrase !== null) {
+      getString += `&passphrase=${encodeURIComponent(passPhrase.trim()).replace(
+        /%20/g,
+        "+"
+      )}`;
+    }
+    const signature = crypto.createHash("md5").update(getString).digest("hex");
+    res.status(200).json({
+      status: "200",
+      message: "Signature created",
+      data: signature,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "500",
+      message: "Unexpected error: " + err,
+    });
+  }
 });
 
 async function startServer() {
--- loaders/routes.js
+++ loaders/routes.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const Routes = require("../routes/index");
 
 module.exports = function (app) {
   app.use(express.json());
-  app.use("/", Routes);
 };
